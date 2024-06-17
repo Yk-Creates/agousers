fa32147378360339e3406a0b4ddd4bf4a63ad185
Yogesh Vashisth <writetokhair@gmail.com>
2024-06-17 12:48:59 +0530

    state management done

--- App.tsx
+++ App.tsx
@@ -2,13 +2,16 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigation from './src/navigation/AppNavigation';
-
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+const queryClient = new QueryClient();
 const App = () => {
   return (
     <>
-      <NavigationContainer>
-        <AppNavigation />
-      </NavigationContainer>
+      <QueryClientProvider client={queryClient}>
+        <NavigationContainer>
+          <AppNavigation />
+        </NavigationContainer>
+      </QueryClientProvider>
     </>
   );
 };
--- src/screen/landing/Landing.tsx
+++ src/screen/landing/Landing.tsx
@@ -1,14 +1,49 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, ActivityIndicator} from 'react-native';
 import React from 'react';
+import useGetTextData from '../../hooks/useGetTestData';
+
 
 const Landing = () => {
+  const {data, error, isLoading} = useGetTextData();
+  
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" color="#ffffff" />
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.errorText}>Error loading data</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={{flex: 1, backgroundColor: '#0C0C0C'}}>
-      <Text>landing</Text>
+    <View style={styles.container}>
+      <Text style={styles.dataText}>{data?.message}</Text>
     </View>
   );
 };
 
 export default Landing;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0C0C0C',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  dataText: {
+    color: '#ffffff',
+    fontSize: 18,
+  },
+  errorText: {
+    color: 'red',
+    fontSize: 18,
+  },
+});
