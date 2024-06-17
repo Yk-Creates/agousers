29d3400237eb32542224228e3e26fff709d37966
kartik sharma <kartik200327@gmail.com>
2024-06-17 14:57:35 +0530

    please check navigation issue in main landing page so that i can work on login/signup page

--- src/screen/landing/Landing.tsx
+++ src/screen/landing/Landing.tsx
@@ -1,10 +1,13 @@
-import {StyleSheet, Text, View, ActivityIndicator} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
+import { ActivityIndicator, Dimensions, Image, StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import useGetTextData from '../../hooks/useGetTestData';
 
 
 const Landing = () => {
   const {data, error, isLoading} = useGetTextData();
+  const navigation = useNavigation();
   
   if (isLoading) {
     return (
@@ -24,26 +27,58 @@ const Landing = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.dataText}>{data?.message}</Text>
+      <Image
+        source={require('../../assets/images/ago.png')}
+        style={styles.image}
+      />
+    <TouchableOpacity onPress={() => navigation.navigate('login')}> 
+        <Image
+          source={require('../../assets/images/rightarrow.png')}
+          style={styles.rightArrow}
+        />
+      </TouchableOpacity>
+      <Image
+        source={require('../../assets/images/bottom_design.png')}
+        style={styles.bottomImage}
+      />
     </View>
   );
 };
 
 export default Landing;
-
+const { width: screenWidth } = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#0C0C0C',
+    backgroundColor: '#fff',
     justifyContent: 'center',
     alignItems: 'center',
   },
   dataText: {
-    color: '#ffffff',
+    color: '#000',
     fontSize: 18,
   },
   errorText: {
     color: 'red',
     fontSize: 18,
+  },
+  image: {
+
+    width: 250,
+    height: 250,
+    marginBottom: 50,
+  },
+  rightArrow: {
+    cursor: 'pointer',
+    width: 50,
+    height: 50,
+    marginBottom: 20,
+  },
+  bottomImage: {
+    position: 'absolute',
+    bottom: 0,
+    width: screenWidth,
+    height: 500,
+    resizeMode: 'cover',
   },
 });
